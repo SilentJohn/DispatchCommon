b026ffd52b0552630b15f0b3509e8e5c98748ef0
Jiang, John X. -ND <John.X.Jiang.-ND@disney.com>
2020-10-18 18:39:08 +0800

    Page view added.

---
+++ DispatchCommon/DispatchCommon/DispatchCommon.h
@@ -0,0 +1,18 @@
+//
+//  DispatchCommon.h
+//  DispatchCommon
+//
+//  Created by Jiang, John X. -ND on 10/18/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DispatchCommon.
+FOUNDATION_EXPORT double DispatchCommonVersionNumber;
+
+//! Project version string for DispatchCommon.
+FOUNDATION_EXPORT const unsigned char DispatchCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DispatchCommon/PublicHeader.h>
+
+
